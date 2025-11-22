7f9fa6f1eb94bf8ad56d5d0c4aed2c4cff6e5870
kilik42 <marvinevins@yahoo.com>
2025-11-21 18:24:00 -0600

    working on ssh and allow ports

--- sg.tf
+++ sg.tf
@@ -53,10 +53,32 @@ resource "aws_security_group_ingress_rule" "allow_http" {
     from_port   = 80
     to_port     = 80
     protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
+    cidr_blocks = ["0.0.0.0/0"] # allow from anywhere this is set to this range because its http traffic
+    
   } 
   tags{
     Name = "allow_http_access for security_group_1"
+  }
+}
+
+
+#egress rule to allow all outbound traffic
+# why i would need this if egress is already defined in security group?
+# The egress block in the security group defines the default outbound rules, but this allows for more granular control.
+resource "aws_security_group_egress_rule" "allow_all_outbound" {   
+  security_group_id = aws_security_group.security_group_1.id
+  description       = "Allow all outbound traffic"
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    # -1 means all protocols
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]   
+
+  }
+  tags{
+    Name = "allow_all_outbound for security_group_1"
   }
 }
 
