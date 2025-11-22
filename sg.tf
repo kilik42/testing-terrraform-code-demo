41ac74937ee9b5f2e70e7b1691ccc4d949d856ee
kilik42 <marvinevins@yahoo.com>
2025-11-21 18:19:29 -0600

    working on ssh and allow ports

--- sg.tf
+++ sg.tf
@@ -21,8 +21,49 @@ resource "aws_security_group" "security_group_1" {
 
 
 
+resource "aws_security_group_ingress_rule" "allow_sg_ssh" {   
+  security_group_id = aws_security_group.security_group_1.id
+  description       = "Allow SSH access from anywhere"
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  tags{
+    Name = "allow_ssh_access for security_group_1"
+  }
+
+  # egress {
+  #   from_port   = 0
+  #   to_port     = 0
+  #   protocol    = "-1"
+  #   cidr_blocks = ["0.0.0.0/0"]
+  # }
+}
+
+
+# ingress rule to allow HTTP traffic
+resource "aws_security_group_ingress_rule" "allow_http" {   
+  security_group_id = aws_security_group.security_group_1.id
+  description       = "Allow HTTP access from anywhere"
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  } 
+  tags{
+    Name = "allow_http_access for security_group_1"
+  }
+}
+
 # outputs for security groups
 output "security_group_1_id" {
   description = "The ID of security group 1"
   value       = aws_security_group.security_group_1.id
 }
+
+
